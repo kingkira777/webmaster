b57278787c2e22a94143055e2718470107ddba7f
King <hsouleater@gmail.com>
2019-11-21 04:25:28 +0800

    xdata

--- routes/docusign/docusign.js
+++ routes/docusign/docusign.js
@@ -61,8 +61,7 @@ router.get('/signed-docs',function(req,res,next){
 //Open Document
 router.get('/open-document?',function(req,res,next){
     if(req.session.branch){
-        res.setHeader('Access-Control-Allow-Origin', "*");
-        res.setHeader('Access-Control-Allow-Headers', "*");
+        res.setHeader('Access-Control-Allow-Origin', "http://127.0.0.1:1001");
         res.render('docusign/open_docu',{title : 'Open Document', accesslvl : req.session.accesslvl});
         res.end();
     }else{
