bf50a3ca07ea099ab952ad9d2d47c1248bde468d
King <hsouleater@gmail.com>
2019-11-25 06:39:07 +0800

    new page update

--- routes/front-web/index.js
+++ routes/front-web/index.js
@@ -3,12 +3,27 @@ var router = express.Router();
 var master = require('../modules/master');
 
 
+//=================================CLIENT SIDE================================//
+
+//Home Page 
 router.get('/',function(req,res,next){
     res.render('front-web/index',{title: 'Hospice Fusion'});
     res.end();
 });
 
 
+//Contact Page
+router.get('/contact', function(req,res,next){
+    res.render('front-web/contact',{ title : 'Contact'});
+    res.end();
+});
+
+//About Page
+router.get('/about',function(req,res,next){
+    res.render('front-web/about', { tilte : 'About' })
+});
+
+//Login & Regiser Page
 router.get('/login',function(req,res,next){
     res.render('front-web/login',{ title : 'Login' });
     res.end();    
