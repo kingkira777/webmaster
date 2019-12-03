de01b7833e7d633440acbbe33dc87b7120ff0836
King <hsouleater@gmail.com>
2019-12-04 02:29:48 +0800

    update connection

--- routes/modules/connection.js
+++ routes/modules/connection.js
@@ -3,19 +3,19 @@ var con;
 
 function Connetion(){
 
-    con = mysql.createPool({
-        host : 'localhost',
-        user : 'root',
-        password : 'admin',
-        database :  'master'
-    }); 
-
     // con = mysql.createPool({
-    //     host : '182.50.132.78',
-    //     user : 'master',
-    //     password : 'master707!',
+    //     host : 'localhost',
+    //     user : 'root',
+    //     password : 'admin',
     //     database :  'master'
     // }); 
+
+    con = mysql.createPool({
+        host : '182.50.132.78',
+        user : 'master',
+        password : 'master707!',
+        database :  'master'
+    }); 
 
     return con; 
 }
